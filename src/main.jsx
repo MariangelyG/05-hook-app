dd3d2ad2a7cf4a199ecddd11444ade90f9f75ace
MariangelyG <mariangelygonzalez90@gmail.com>
2023-09-21 20:48:07 -0300

    "useEffect"

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 // import { HooksApp } from './hooksApp'
 // import { CounterApp } from './01-useState/CounterApp'
-import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
+// import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
+import { SimpleForm } from './02-useEffect/SimpleForm'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <CounterWithCustomHook/>
-  </React.StrictMode>,
+  // <React.StrictMode>
+    <SimpleForm/>
+  //  { </React.StrictMode>, }
 )
