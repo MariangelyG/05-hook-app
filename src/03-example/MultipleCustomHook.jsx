2309d13a86a99961583e2a4de6630802793a39dd
MariangelyG <mariangelygonzalez90@gmail.com>
2023-09-25 23:12:57 -0300

    "useFetch-CustomHook"

---
+++ src/03-example/MultipleCustomHook.jsx
@@ -0,0 +1,36 @@
+import { useFetch } from "../hooks/useFetch"
+
+
+export const MultipleCustomHook = () => {
+
+    const {data, isLoading, hasError} = useFetch('https://thesimpsonsquoteapi.glitch.me/quotes');
+    const { author, quote } = !!data && data[0]
+
+  return (
+    <>
+        <h1>BreakingBad Quotes</h1>
+        <hr />
+
+        {
+            isLoading
+                ? (
+                    <div className="alert alert-info text-center">
+                        Loading...
+                    </div>
+                  )
+                : (
+                    <blockquote className="blockquote text-end">
+                    <p className="mb-1">{ quote }</p>
+                    <footer className="blockquote-footer">{ author }</footer>
+                    </blockquote>
+                   )  
+        
+        }
+
+            <button className="btn btn-primary">
+                Next quote
+            </button>
+
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import './index.css'
 // import { HooksApp } from './hooksApp'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
-import { SimpleForm } from './02-useEffect/SimpleForm'
+// import { SimpleForm } from './02-useEffect/SimpleForm'
+// import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
+import { MultipleCustomHook } from './03-example/MultipleCustomHook'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <SimpleForm/>
+  < MultipleCustomHook/>
   //  { </React.StrictMode>, }
 )
