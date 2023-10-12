9c1aedb155e3f65e0ea70a7365f02f9629bbea4c
MariangelyG <mariangelygonzalez90@gmail.com>
2023-10-12 18:57:28 -0300

    "Guardar y leer  LocalStorage"

---
+++ src/08-useReducer/TodoApp.jsx
@@ -0,0 +1,66 @@
+import { useEffect, useReducer } from "react"
+import { todoReducer } from "./todoReducer"
+import { TodoList } from "./TodoList"
+import { TodoAdd } from "./TodoAdd"
+
+const initialState = [
+    // {
+    //     id: new Date().getTime(),
+    //     description: 'Recopectar la piedra del alma',
+    //     done: false,
+    // },
+    // {
+    //     id: new Date().getTime() *3,
+    //     description: 'Recopectar la piedra del tiempo',
+    //     done: false,
+    // },
+
+]
+
+const init = () =>{
+    return JSON.parse(localStorage.getItem('todos')) || [];
+}
+
+
+export const TodoApp = () => {
+  
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos) )
+    }, [todos])
+    
+
+    const handleNewTodo = ( todo ) =>{
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo
+        }
+        dispatch(action)
+    }
+  
+    return (
+
+    <>
+        <h1>TodoApp: 10, <small>pendientes: 2</small></h1>
+        <hr />
+        <div className="row">
+            <div className="col-7">
+                <TodoList todos={ todos } />
+             </div>
+
+            <div className="col-5">
+                <h4>Agregar TODO</h4>
+                <hr />
+
+                <TodoAdd onNewTodo={handleNewTodo}/>
+                
+            </div>
+        </div>
+
+
+    
+    
+    </>
+  )
+}
